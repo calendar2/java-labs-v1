1887a2e913a6a125c453fdc70efe3fe9b619fedd
calendar2 <zoro94317@gmail.com>
2025-04-28 16:04:19 +0900

    TIL: 250428

--- chapter1/labs/lab2/DataTypesLab.java
+++ chapter1/labs/lab2/DataTypesLab.java
@@ -1,5 +1,7 @@
 package chapter1.labs.lab2;
 
+import java.util.Arrays;
+
 /**
  * Lab 2: 자바 데이터 타입 실습
  * 
@@ -9,22 +11,36 @@ public class DataTypesLab {
     public static void main(String[] args) {
         // TODO: 다음 기본 데이터 타입의 변수를 선언하고 값을 할당하세요.
         // 1. 정수형(byte, short, int, long) 변수 선언 및 할당
-        
+        byte a = 127;
+        short b = 2000;
+        int c = 300000;
+        long d = 400000L;
         
         // TODO: 2. 실수형(float, double) 변수 선언 및 할당
-        
+        float e = 5.0f;
+        double f = 5.03;
         
         // TODO: 3. 문자형(char) 변수 선언 및 할당
-        
+        char g = 'a';
         
         // TODO: 4. 논리형(boolean) 변수 선언 및 할당
-        
+        boolean h = true;
         
         // TODO: 5. 모든 변수 값을 출력하세요.
-        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
         
         // TODO: 6. 자신의 키(cm)와 몸무게(kg)를 저장하는 변수를 선언하고 
         // BMI 지수를 계산하여 출력하세요. (BMI = 몸무게(kg) / (키(m) * 키(m)))
-        
+        short height = 174;
+        byte weight = 52;
+        float BMI = weight / (((float) height / 100) * ((float) height / 100));
+        System.out.println("BMI = " + BMI);
     }
 } 
--- chapter1/labs/lab3/TypeCastingLab.java
+++ chapter1/labs/lab3/TypeCastingLab.java
@@ -10,23 +10,47 @@ public class TypeCastingLab {
         // TODO: 1. 자동 형변환(Implicit Casting) 예제
         // byte -> int, int -> long, long -> float, float -> double 순으로 
         // 변수를 선언하고 자동 형변환을 수행한 뒤 결과 출력하기
-        
+        byte a = 10;
+        int b = a;
+        long c = b;
+        float d = c;
+        double f = d;
+        System.out.println(f);
         
         // TODO: 2. 명시적 형변환(Explicit Casting) 예제
         // double -> float -> long -> int -> char -> short -> byte 순으로
         // 변수를 선언하고 명시적 형변환을 수행한 뒤 결과 출력하기
-        
+        f = 12.5432;
+        d = (float) f;
+        c = (long) d;
+        b = (int) c;
+        char g = (char) b;
+        short h = (short) g;
+        a = (byte) h;
+        System.out.println(a);
         
         // TODO: 3. 데이터 손실 확인하기
         // int 값을 byte로 변환하여 데이터 손실이 발생하는 예제 작성하기
-        
+        b = 3000;
+        a = (byte) b;
+        System.out.println(a);
         
         // TODO: 4. 문자열과 숫자 간 변환하기
         // 문자열 "123"을 정수로, "3.14"를 실수로 변환하기
-        
+        String ex1 = "123";
+        String ex2 = "3.14";
+        int ex1_change = Integer.parseInt(ex1);
+        double ex2_change = Double.parseDouble(ex2);
+        System.out.println("ex1_change = " + ex1_change);
+        System.out.println("ex2_change = " + ex2_change);
         
         // TODO: 5. 숫자를 문자열로 변환하기
         // 정수 456과 실수 7.89를 문자열로 변환하기
-        
+        int ex3 = 456;
+        double ex4 = 7.89;
+        String ex3_change = "" + ex3;
+        String ex4_change = "" + ex4;
+        System.out.println("ex3_change = " + ex3_change);
+        System.out.println("ex4_change = " + ex4_change);
     }
 } 
